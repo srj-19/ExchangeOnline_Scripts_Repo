7080028d2d1cbb846539ca757682c5832c41e1c6
srj-19 <106163955+srj-19@users.noreply.github.com>
2022-05-25 09:50:54 +0530

    SharedMailboxAccess
    This script requests and accepts Shared mailbox and User mailbox email addresses, and validates access to the Shared mailbox. Further, it provides an option to Grant permissions to user on shared mailbox if it's not present already.

---
+++ SharedMailboxAccess.ps1
@@ -0,0 +1,29 @@
+﻿$Shared = Read-Host -Prompt "Enter Shared Mailbox Email"
+$user = Read-Host -Prompt "Enter User Email"
+$status = Get-Mailbox $Shared -ErrorAction SilentlyContinue
+if($status -eq $null)
+{
+    Write-Host "`n$Shared could not be found OR does not exist" -ForegroundColor Yellow
+}
+else
+{
+    $users = Get-MailboxPermission $Shared | select -ExpandProperty user
+    if($users -contains $user)
+    {
+        Write-Host "`n$user already has access to $Shared`n" -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host "`n$user does not have access to $Shared`n" -ForegroundColor Yellow
+        Write-Host "1. Grant Access"
+        Write-Host "2. Exit"
+        $input = Read-Host -Prompt "`nSelect action from above (1 or 2)"
+        switch($input)
+        {
+            1 {Add-MailboxPermission $Shared -User $user -AccessRights fullaccess
+               Add-RecipientPermission $Shared -Trustee $user -AccessRights sendas -Confirm:$false;break}
+            2 {;break}
+        }
+    }
+}
+
